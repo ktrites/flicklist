64f60b233264e6e35041c2b1a3dc035bc93cca49
Kathryn Trites <ktrites@gmail.com>
2016-08-30 03:00:46 +0000

    finish Flicklist 0 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -2,7 +2,7 @@
 
 var api = {
 	root: "https://api.themoviedb.org/3",
-	token: "" // TODO put your api key here
+	token: "9998669cfab84d19ba8945ad0337207e" // TODO put your api key here  DONE!
 }
 
 /**
